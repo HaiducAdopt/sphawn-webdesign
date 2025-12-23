6d18daafa2d0b4866ac918c6711a13e3fd260471
haiducadopt <stefan.plesa@gmail.com>
2025-12-23 15:57:07 +0100

    offerte

--- recover/page.tsx
+++
@@ -1,6 +0,0 @@
-// app/[locale]/offers/page.tsx
-import Page, { generateMetadata } from "../../offers/page";
-export { generateMetadata };
-export default function OffersLocalePage() {
-  return <Page />;
-}
